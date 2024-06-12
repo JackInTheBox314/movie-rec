959f783b23a6cb305a62268d5ab558a1a851861b
Jack Wang <jwangjy315@gmail.com>
2024-06-12 15:26:28 -0400

    added registration page

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ app = Flask(__name__, template_folder='templates', static_folder='public', stati
 def index():
     return render_template('login.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=18686, debug=True)

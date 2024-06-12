3cbb244b78551494e879e71c478e0f856c854f7d
Jack Wang <jwangjy315@gmail.com>
2024-06-12 14:44:31 -0400

    added login page and css

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request, make_response, render_template, Response
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='public', static_url_path='/')
+
+# scrape movie info off imdb
 
 @app.route('/')
 def index():
-    return "<h1>Hello World</h1>"
+    return render_template('login.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    app.run(host='0.0.0.0', port=18686, debug=True)

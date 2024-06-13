004142846c4a93f5b721b454934b915ca56c10bc
Jack Wang <jwangjy315@gmail.com>
2024-06-13 18:42:44 -0400

    began database implementation

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, request, make_response, render_template, Response
+from db import AtlasClient
 
 app = Flask(__name__, template_folder='templates', static_folder='public', static_url_path='/')
 
+ATLAS_URI = "mongodb+srv://jwangjy315:kkEQFR16wYGD95H3@cluster0.c4k5pnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+DB_NAME = 'sample_mflix'
+COLLECTION_NAME = 'embedded_movies'
+
+atlas_client = AtlasClient (ATLAS_URI, DB_NAME)
+atlas_client.ping()
+print ('Connected to Atlas instance! We are good to go!')
+
+
+
 # scrape movie info off imdb
+
+
 
 @app.route('/')
 def index():
